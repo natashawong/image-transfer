2338f3c17181fba255a3b62066e6c63782ad48d1
natashawong <natasha.wong10@gmail.com>
2021-03-20 07:38:56 +0800

    added upload button and display preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
-    render() {
-        return (
-          <div>
-            <h1>Your image style transfer result is: </h1>
-            <img src={"http://0ef631042aaa.ngrok.io/result" || null}/>
-          </div>
-        )
+  constructor(props){
+    super(props)
+    this.state = {
+      image: null
     }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      image: URL.createObjectURL(event.target.files[0])
+    })
+  }
+
+  render() {
+      return (
+        <div>
+          <h1>Upload an image that you want to be changed into your chosen style:</h1>
+          <input type="file"  accept="image/jpeg, image/png" name="image" id="file" onChange={this.handleChange}/>
+          <img src={this.state.image}/>
+          <h1>Your image style transfer result is: </h1>
+          <img src={"http://0ef631042aaa.ngrok.io/result" || null}/>
+        </div>
+      )
+  }
 }
