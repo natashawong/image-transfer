a6481011dfdf0fb8dccd8dc8f91e939227b30832
natashawong <natasha.wong10@gmail.com>
2021-03-26 09:55:08 +0800

    frontend updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, {Component} from 'react';
 
+const axios = require('axios')
+
 export default class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      image: null
+      image: null,
+      loading: 0
     }
   }
 
   handleChange = (event) => {
+    const formData = new FormData();
     this.setState({
       image: URL.createObjectURL(event.target.files[0])
     })
-  }
+    formData.append("image", event.target.files[0])
 
+    axios.post('http://c3363ecd6fb9.ngrok.io/stylize', formData)
+    .then(resp => console.log(resp))
+    .then(resp => this.setState({loading: 1}))
+  }
+  
   render() {
       return (
         <div>
           <h1>Upload an image that you want to be changed into your chosen style:</h1>
-          <input type="file"  accept="image/jpeg, image/png" name="image" id="file" onChange={this.handleChange}/>
+          <input type="file"  encType="multipart/form-data" accept="image/jpeg, image/png" name="image" id="file" onChange={this.handleChange}/>
           <img src={this.state.image}/>
           <h1>Your image style transfer result is: </h1>
-          <img src={"http://0ef631042aaa.ngrok.io/result" || null}/>
+          <img src={this.state.loading == 1 ? "http://c3363ecd6fb9.ngrok.io/result" : null}/>
         </div>
       )
   }
